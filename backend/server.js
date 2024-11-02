d70c4b51ec19bfeef025d71015cde7bd93dad035
carlos <camr.personal@gmail.com>
2024-11-01 19:27:40 -0500

    signup jsonwebtoken added.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require("mongoose")
 const app = express()
 const workoutRoutes = require("./routes/workouts")
+const usersRoutes = require("./routes/users")
 
 app.use(express.json())
 app.use((req,res,next) =>{
@@ -10,6 +11,7 @@ app.use((req,res,next) =>{
     next()    
 })
 app.use('/api/workouts',workoutRoutes)
+app.use('/api/users',usersRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
